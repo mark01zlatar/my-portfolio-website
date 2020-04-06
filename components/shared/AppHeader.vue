7103d024e4c49ee6d6a39fab065faf51a1a2a94b
Marko Zlatar <markozlatar01@gmail.com>
2020-04-06 19:11:21 +0200

    Added: Hamburger icon

--- components/shared/AppHeader.vue
+++ components/shared/AppHeader.vue
@@ -4,22 +4,28 @@
       <nuxt-link tag="a" to="/">
         <img src="@/assets/images/MZ-logo-white-small.png" width="70" alt="Marko Zlatar Logo White" />
       </nuxt-link>
-      <ul class="navigation-list">
-        <nuxt-link to="#" tag="li">About me</nuxt-link>
-        <nuxt-link to="#" tag="li">Projects</nuxt-link>
-        <nuxt-link to="#" tag="li">Technologies</nuxt-link>
-      </ul>
-      <nuxt-link to="#" class="resume-download" tag="a">Resume</nuxt-link>
-      <app-button text="Contact me"></app-button>
+      <div class="desktop-navigation">
+        <ul class="navigation-list">
+          <nuxt-link to="#" tag="li">About me</nuxt-link>
+          <nuxt-link to="#" tag="li">Projects</nuxt-link>
+          <nuxt-link to="#" tag="li">Technologies</nuxt-link>
+        </ul>
+        <nuxt-link to="#" class="resume-download" tag="a">Resume</nuxt-link>
+        <app-button text="Contact me"></app-button>
+      </div>
+      <app-hamburger class="hamburger"></app-hamburger>
     </nav>
   </header>
 </template>
 
 <script>
 import AppButton from '@/components/shared/AppButton'
+import AppHamburger from '@/components/shared/AppHamburger'
+
 export default {
   components: {
-    AppButton
+    AppButton,
+    AppHamburger
   }
 }
 </script>
@@ -45,10 +51,17 @@ header {
   }
 }
 
+.desktop-navigation {
+  margin-left: 120px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 .navigation-list {
   color: white;
   display: flex;
-  margin-left: 120px;
   li {
     margin-right: 30px;
     transition: color 200ms ease;
@@ -65,6 +78,20 @@ header {
   transition: color 200ms ease;
   &:hover {
     color: $site-text-dark;
+  }
+}
+
+.hamburger {
+  margin-left: auto;
+  display: none;
+}
+
+@media screen and (max-width: 1000px) {
+  .desktop-navigation {
+    display: none;
+  }
+  .hamburger {
+    display: block;
   }
 }
 </style>
