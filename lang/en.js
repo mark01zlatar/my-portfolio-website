41dcae9f32dee245f98bc4b2dc0298560c18760e
Marko Zlatar <markozlatar01@gmail.com>
2020-04-16 18:51:51 +0200

    Fixed: Typos in project section

--- lang/en.js
+++ lang/en.js
@@ -55,11 +55,11 @@ export default {
     aboutSection: {
       title: 'My story',
       firstPara:
-        'I was never the best at school or academics, I’m just a super passionate and hard working guy. Most of my time I spend building websites and web apps, but there’s also a lot more to it. Also I spent at least one hour a day working out or having some physical activity - for me health is most important thing, and then comes everything else.',
+        'I was never the best at school or academics, I’m just a super passionate and hard-working guy. Most of my time I spend building websites and web apps, but there’s also a lot more to it. Also I spent at least one hour a day working out or having some physical activity - for me health is the most important thing, and then comes everything else.',
       secondPara:
-        'I started my journey as a self-taught web and software developer in second year of High School. Ever since I have fallen in love with programming and spend most of my days taking my craft and skills to the next level.',
+        'I started my journey as a self-taught web and software developer in the second year of High School. Ever since I have fallen in love with programming and spend most of my days taking my craft and skills to the next level.',
       thirdPara:
-        'Nowadays I tend to code all of my projects from scratch in order to deliver a unique experience to every company. I spend my free time learning and using what i learn by building new and challenging projects.',
+        'Nowadays I tend to code all of my projects from scratch in order to deliver a unique experience to every company. I spend my free time learning and using what I learn by building new and challenging projects.',
     },
     projectsSection: {
       title: 'Portfolio',
@@ -68,22 +68,22 @@ export default {
       gemMedia: {
         title: 'Gem Media - Social media project',
         overview:
-          'This project was created from two separate parts - frontend and backend. For frontend I used Nuxt.js to create SSR(Server Side Render) Vue.js application. For UI i used Vuetify.js(Material Design component framework). For backend I created REST API with Node.js(Express framework). For database MongoDB(NoSQL database) is used. For  interactions with MongoDB mongoose library is used. To implement private messaging feature Socket.io library is used .Node.js REST API is tested with Mocha.js, Chai.js, Sinon.js...Also rate-limiting is implemented.',
+          'This project was created from two separate parts - frontend and backend. For frontend, I used Nuxt.js to create SSR(Server Side Render) Vue.js application. For UI I used Vuetify.js(Material Design component framework). For backend, I created a REST API with Node.js(Express framework). For database MongoDB(NoSQL database) is used. For interactions with MongoDB, a mongoose library is used. To implement the private messaging feature Socket.io library is used .Node.js REST API is tested with Mocha.js, Chai.js, Sinon.js...Also, rate-limiting is implemented.',
       },
       wlfClothing: {
         title: 'WLF(We love fashion) Clothing - eCommerce project',
         overview:
-          'This project was created from two separate parts - frontend and backend. For frontend I used Nuxt.js to create SSR(Server Side Render) Vue.js application. For UI i used Vuetify.js(Material Design component framework). For backend I created REST API with Node.js(Express framework). For database MongoDB(NoSQL database) is used. For  interactions with MongoDB mongoose library is used. Some of features that are implemented are: Authentication & Authorization, Payment with paypal, CMS for users and products, pagination, email interactions, AWS S3 storage... basically everything one e-commerce must have.',
+          'This project was created from two separate parts - frontend and backend. For frontend I used Nuxt.js to create SSR(Server Side Render) Vue.js application. For UI I used Vuetify.js(Material Design component framework). For backend, I created a REST API with Node.js(Express framework). For database MongoDB(NoSQL database) is used. For interactions with MongoDB mongoose library is used. Some of the features that are implemented are: Authentication & Authorization, Payment with PayPal, CMS for users and products, pagination, email interactions, AWS S3 storage... basically everything one e-commerce must-have.',
       },
       seoMind: {
         title: 'SEO Mind - Website project',
         overview:
-          'To create this project, first I used tool created by Nuxt.js team called create-nuxt-app. In process of creating web application, for style is used Sass(Scss) and for version control is used Git & Github',
+          'To create this project, first I used a tool created by the Nuxt.js team called create-nuxt-app. In process of creating a web application, for style is used Sass(Scss) and for version control is used Git & Github.',
       },
       greatAthlete: {
         title: 'Great Athlete - GYM website project',
         overview:
-          'To create this project, first I created setup with Webpack(module bundler). Once webpack setup is done correctly, he is responsible for fast development process and bundled minified code in production. Sass is used as css extension so i was able to to reuse css and organize it well. For all interactions with user vanilla JavaScript was used. Git and Github were used for version control.',
+          'To create this project, first I created a setup with Webpack(module bundler). Once webpack setup is done correctly, he is responsible for the fast development process and bundled minified code in production. Sass is used as a CSS extension so I was able to reuse CSS and organize it well. For all interactions with user vanilla JavaScript was used. Git and Github were used for version control.',
       },
     },
     technologiesSection: {
